46fec143b6216965f00962a3bfa42b3bf74f2b08
Kevin Ho <johnwater007@gmail.com>
2018-02-05 16:47:24 +0800

    Add uniqueness validates of name column

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,11 @@ class User < ApplicationRecord
   # 並參考 Devise 文件自訂表單後通過 Strong Parameters 的方法
   validates_presence_of :name
   # 加上驗證 name 不能重覆 (關鍵字提示: uniqueness)
-  
+  validates_uniqueness_of :name
 
   # 回傳 current_user是否為使用者？
   def admin?
     self.role == "admin"
-  end
+  enda
 
 end
