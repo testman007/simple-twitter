b96e94084e166c8524f89d83fe4cc27eb67d53ce
Kevin Ho <johnwater007@gmail.com>
2018-02-05 17:09:39 +0800

    add update method for users controller, update users/edit.html.erb, new users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :user_params, only: [:update]
 
   def tweets
   end
 
   def edit
+    # before_action :set_user
   end
 
   def update
+    # before_action :set_user
+    if @user.id == current_user.id
+      if @user.update(user_params)
+        flash[:info] = "User Profile was successfully updated"
+        redirect_to user_path(@user)
+      else
+        flash.now[:info] = "User Profile was failed to updated"
+        render :edit
+      end
+    else
+      flash.now[:info] = "You have no right to change this page"
+      render :edit
+    end
   end
 
   def followings
@@ -19,6 +35,18 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  #
+  # Private
+  #
+
+  def user_params
+    params.require(:user).permit(:email, :name, :avatar, :introduction)
+  end
+
+  def set_user
+    @user = User.find(params[:id])    
   end
 
 end
